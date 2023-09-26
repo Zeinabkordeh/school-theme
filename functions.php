d6f0254e28099791046e4c949464cb50f5a30603
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2023-09-26 13:13:22 -0700

    added replacement for ...

--- functions.php
+++ functions.php
@@ -222,3 +222,12 @@ function change_student_title_placeholder($title) {
 }
 add_filter('enter_title_here', 'change_student_title_placeholder');
 //gathered with help from chatGPT
+
+function custom_excerpt_more($more) {
+	if(is_singular('taze-student')){
+		return ' <a href="' . get_permalink() . '">Read More about the Student</a>';
+	} else{
+		return $more;
+	}
+}
+add_filter('excerpt_more', 'custom_excerpt_more');
--- template-parts/content-taze-student.php
+++ template-parts/content-taze-student.php
@@ -7,46 +7,42 @@ get_header();
 
 // Custom query to retrieve students
 $args = array(
-    'post_type' => 'taze-student', // Replace with your custom post type name
-    'posts_per_page' => -1, // Retrieve all students
-    'orderby' => 'title', // Order by student's first name
-    'order' => 'ASC', // Sort in ascending order
+    'post_type' => 'taze-student', 
+    'posts_per_page' => -1, 
+    'orderby' => 'title', 
+    'order' => 'ASC', 
 );
 
 $query = new WP_Query($args);
 
-if(!is_singular()) :
-    if ($query->have_posts()) :
-        while ($query->have_posts()) :
-            $query->the_post();
+if ($query->have_posts()) :
+    while ($query->have_posts()) :
+        $query->the_post();
 
-            // Get student's information
-            $student_name = get_the_title();
-            $student_excerpt = wp_trim_words(get_the_excerpt(), 25); //ask chat gpt how to splice exerpt to 25 words
-            $student_image = get_the_post_thumbnail();
-            $student_link = get_permalink();
+        $student_name = get_the_title();
+        $student_excerpt = get_the_excerpt(); 
+        $student_image = get_the_post_thumbnail();
+        $student_link = get_permalink();
 
-            // Output student information
-            ?>
-            <div class="student-entry">
-                <h2><?php echo esc_html($student_name); ?></h2>
-                <div class="student-thumbnail">
-                    <?php echo $student_image; ?>
-                </div>
-                <div class="student-content">
-                    <p><?php echo esc_html($student_excerpt); ?></p>
-                    <p><a href="<?php echo esc_url($student_link); ?>">Read More about the Student</a></p>
-                </div>
+        $student_excerpt = str_replace('[&hellip;]', ' <a href="' . esc_url($student_link) . '">Read More about the Student</a>', $student_excerpt);
+        //function found using Chat GPT
+    
+        ?>
+        <div class="student-entry">
+            <h2><?php echo esc_html($student_name); ?></h2>
+            <div class="student-thumbnail">
+                <?php echo $student_image; ?>
             </div>
-            <?php
-        endwhile;
-        wp_reset_postdata();
-    else :
-        echo 'No students found.';
-    endif;
+            <div class="student-content">
+                <p><?php echo wp_kses_post($student_excerpt); ?></p> //wp_kses_post sanitizes html
+            </div>
+        </div>
+        <?php
+    endwhile;
+    wp_reset_postdata();
 else :
-    the_title();
-    the_content();
+    echo 'No students found.';
 endif;
 
 get_footer();
+?>

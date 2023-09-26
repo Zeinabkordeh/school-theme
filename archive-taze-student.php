87def9a08f5465670283037ae181f081d5fcb0f3
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2023-09-25 18:59:47 -0700

    added stuff

--- archive-taze-student.php
+++ archive-taze-student.php
@@ -32,7 +32,6 @@ get_header();
 			<?php
 			/* Start the Loop */
             if(have_posts() ){
-                while ( have_posts() ) :
                     the_post();
     
                     /*
@@ -41,7 +40,7 @@ get_header();
                      * called content-___.php (where ___ is the Post Type name) and that will be used instead.
                      */
                     get_template_part( 'template-parts/content', get_post_type() );
-                endwhile;
+               
                 wp_reset_postdata();
             }
 

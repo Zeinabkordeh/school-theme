a690cfbb6331849a5c1519081df828cc87d6140e
Zeinab Kordeh <zeinabkordeh@gmail.com>
2023-09-27 12:18:58 -0700

    add

--- single-taze-student.php
+++ single-taze-student.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying a single student post
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
 <?php
 /*
@@ -21,38 +21,40 @@ while (have_posts()) : the_post();
 
     // Get student's information
     $student_name = get_the_title();
+    $student_image = get_the_post_thumbnail();
     $student_link = get_permalink();
 
-    // Output student information for single student post
+    // Output student information for a single student post
     ?>
     <div class="student-entry">
         <h2><?php echo esc_html($student_name); ?></h2>
         <div class="student-thumbnail">
-            <?php the_post_thumbnail('student') ?>
+            <?php echo $student_image; ?>
         </div>
         <div class="student-content">
-            <p><?php echo esc_html(the_content()); ?></p>
+            <p><?php echo esc_html(get_the_content()); ?></p>
         </div>
     </div>
     <?php
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'taze' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'taze' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+    the_post_navigation(
+        array(
+            'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'taze') . '</span> <span class="nav-title">%title</span>',
+            'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'taze') . '</span> <span class="nav-title">%title</span>',
+        )
+    );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+    // If comments are open or we have at least one comment, load up the comment template.
+    if (comments_open() || get_comments_number()) :
+        comments_template();
+    endif;
 
-		endwhile; // End of the loop.
-		?>
+endwhile; // End of the loop.
+?>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
 get_footer();
+?>

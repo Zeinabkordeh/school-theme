26a8e40c534717b4539dced4556b2c94049f0d41
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2023-09-27 19:37:23 -0700

    fixed articel id and class

--- template-parts/content-taze-student.php
+++ template-parts/content-taze-student.php
@@ -4,29 +4,27 @@ Template Name: Student List Template
 */
 
 get_header();
-
 ?>
-<section class="students">
-        <article class="student-entry">
-            <h2><?php echo esc_html(the_title()); ?></h2>
-            <div class="student-thumbnail">
-                <?php the_post_thumbnail('student'); ?>
-            </div>
-            <div class="student-content">
-                <p><?php the_content(); ?></p><br>          
-            </div>
-        </article>
-    <?php
-        the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'taze' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'taze' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-            ?>
 
-</section>
+<article id="post-<?php the_ID();?>" <?php post_class();?>>
+    <h2><?php echo esc_html(the_title()); ?></h2>
+    <div class="student-thumbnail">
+        <?php the_post_thumbnail('student'); ?>
+    </div>
+    <div class="student-content">
+        <p><?php the_content(); ?></p><br>          
+    </div>
+</article>
+
+
 <?php
+
+    the_post_navigation(
+		array(
+			'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'taze' ) . '</span> <span class="nav-title">%title</span>',
+			'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'taze' ) . '</span> <span class="nav-title">%title</span>',
+		)
+	);
 
 get_footer();
 ?>
